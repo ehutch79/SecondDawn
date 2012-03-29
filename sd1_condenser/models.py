40a897ae26d0e8a98d422db6f30d387c65c96560
Eric Hutchinson <ehutch79@gmail.com>
2012-03-29 19:03:46 -0400

    blah

--- sd1_condenser/models.py
+++ sd1_condenser/models.py
@@ -92,7 +92,7 @@ class Faction(models.Model):
         return self.name
 
     def player_members(self):
-        return self.standings.filter(member__is_npc=False,member__is_deceased=False,member__is_retired=False)
+        return self.standings.filter(member__is_npc=False)
 
 
 class Header(models.Model):
