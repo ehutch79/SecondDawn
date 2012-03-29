ce227c89e8d79ef4ba8e386eea466e92d63eac53
Eric Hutchinson <ehutch79@gmail.com>
2012-03-29 19:12:35 -0400

    npc list

--- sd1_condenser/templatetags/condenser_tags.py
+++ sd1_condenser/templatetags/condenser_tags.py
@@ -17,3 +17,18 @@ class GetFactionsNode(template.Node):
         return ''
 
 register.tag('get_faction_list', get_faction_list)
+
+
+def get_npc_list(parser, token):
+    return GetNPCNode()
+
+class GetNPCNode(template.Node):
+    def __init__(self):
+        pass
+
+    def render(self, context):
+        context['npc_list'] = Charecter.objects.filter(is_npc=True)
+        return ''
+
+register.tag('get_faction_list', get_faction_list)
+register.tag('get_npc_list', get_npc_list)
