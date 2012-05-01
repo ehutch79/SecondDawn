d59a124858f522e9f6b8c3818e369c1f248796c5
Eric Hutchinson <ehutch79@gmail.com>
2012-05-01 00:38:05 -0400

    fix

--- sd1_events/views.py
+++ sd1_events/views.py
@@ -17,7 +17,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import F, Q
 from django.utils.timezone import utc
 
-import xhtml2pdf.pisa as xhtml2pdf
+#import xhtml2pdf.pisa as xhtml2pdf
 import stripe
 
 from sd1_events.models import *
