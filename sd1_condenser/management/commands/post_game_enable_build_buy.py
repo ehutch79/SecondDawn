eae83a0348b330375dbf03be8a9c85f0f45c71e2
Eric Hutchinson <ehutch79@gmail.com>
2012-07-31 23:30:27 -0400

    enable just buying build

--- sd1_condenser/management/commands/post_game_enable_build_buy.py
+++ sd1_condenser/management/commands/post_game_enable_build_buy.py
@@ -21,39 +21,24 @@ class Command(BaseCommand):
         event = EventInfo.objects.filter(event_end__lt=now).order_by('-event_end')[0]
         print 'Doing post-game for {event}'.format(event=str(event))
 
-        regs = event.eventregistration_set.filter(attended=True).exclude(user__is_superuser=True)
-        
+        regs = event.eventregistration_set.exclude(user__is_superuser=True)
+ 
         for reg in regs:
             if reg.char:
                 reg.char.can_buy_build = True
                 reg.char.is_new = False
                 reg.char.is_updated = False
                 reg.char.save()
+ 
+#            current_site = Site.objects.get(id=settings.SITE_ID)
 
-                try:
-                    eeps_bank = reg.user.eepsbank
-                except:
-                    eeps_bank = EepsBank()
-                    eeps_bank.user = reg.user
-                    eeps_bank.save()
-
-                EepsBank.objects.filter(pk=eeps_bank.pk).update(eeps=F('eeps')+reg.eeps)
-
-                eeps_record = EepsRecord()
-                eeps_record.user = reg.user
-                eeps_record.eeps = reg.eeps
-                eeps_record.reason = '{event} post game eeps'.format(event=str(event))
-                eeps_record.save()
-
-            current_site = Site.objects.get(id=settings.SITE_ID)
-
-            message = render_to_string('events/admin/event_post_game_email.txt', {'event': event, 'reg': reg, 'site':current_site, })
+#            message = render_to_string('events/admin/event_post_game_email.txt', {'event': event, 'reg': reg, 'site':current_site, })
             
             if not reg.admin_hold:
-
-                send_mail('Second Dawn - {event} report card'.format(event=str(event)), 
-                    message,
-                    settings.EMAIL_FROM,
-                    [reg.user.email], fail_silently=False)
+                pass
+#                send_mail('Second Dawn - {event} report card'.format(event=str(event)), 
+#                    message,
+#                    settings.EMAIL_FROM,
+#                    [reg.user.email], fail_silently=False)
 
 
